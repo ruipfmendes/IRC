8245c61d115ad09873fc30373c2d3dddbc500562
esob <fbasto00@gmail.com>
2014-12-13 22:14:21 +0000

    header

---
+++ header.h
@@ -0,0 +1,28 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <string.h>
+#define SERVER_PORT 9000
+#define BUF_SIZE 1024
+
+typedef struct *lis_util;
+typedef struct utilizador
+{
+	char login[50];
+	char password[50];
+	mensagem 
+	lis_util next; 
+}utilizador;
+
+
+typedef struct mensagem
+{
+	char *destinatario;
+	char *assunto;
+	char *corpo_msg
+	
+}mensagem;
