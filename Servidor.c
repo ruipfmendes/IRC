2c7929110a6a17340f6410ca62889ef9b883204f
esob <fbasto00@gmail.com>
2014-12-13 22:41:25 +0000

    funcao verifica

--- Servidor.c
+++ Servidor.c
@@ -1,1 +1,25 @@
 #include "header.h"
+
+
+void verifica_exis(char* nome, char* pass, lis_util original){
+	lis_util aux;
+	aux = aux->next;
+
+	while(aux != NULL){
+		if (strcmp(aux->login,nome) != 0){
+			printf("Nao existe esse utilizador \n", );
+		}
+		else {
+			if (strcmp(aux->password,pass) != 0){
+				printf("A password introduzida esta errada\n", );
+			}
+			else{
+				return 1
+			}
+		}
+		aux = aux->next;
+
+	}
+	return 0;
+
+}
